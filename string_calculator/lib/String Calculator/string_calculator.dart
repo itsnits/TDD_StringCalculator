38d29a7de6a200e91c08f2148995503b7e31a63b
nishu <nishu1547@gmail.com>
2025-01-28 18:24:22 +0530

    Refactored findDigitsSum function to return sum of two numbers in a string seprated by comma

--- string_calculator/lib/String Calculator/string_calculator.dart
+++ string_calculator/lib/String Calculator/string_calculator.dart
@@ -1,13 +1,18 @@
 class StringCalculator {
   int findDigitsSum(String text, String delimiter) {
+    int res = 0;
     if (text == '') {
       return 0;
     } else {
-      if (int.tryParse(text) != null) {
-        return int.parse(text);
-      } else {
-        throw FormatException('Alphanumeric Values are not allowed: $text');
+      List<dynamic> li = text.split(',');
+      for (var item in li) {
+        if (int.tryParse(item) == null) {
+          throw FormatException('Alphanumeric Values are not allowed: $text');
+        } else {
+          res = res + int.parse(item);
+        }
       }
     }
+    return res;
   }
 }
