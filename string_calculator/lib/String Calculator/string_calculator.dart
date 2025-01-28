b51d26281f8f6695402304915d6935c82ad261c3
nishu <nishu1547@gmail.com>
2025-01-28 19:33:38 +0530

    Refactored findDigitsSum function to handle negative numbers

--- string_calculator/lib/String Calculator/string_calculator.dart
+++ string_calculator/lib/String Calculator/string_calculator.dart
@@ -4,7 +4,18 @@ class StringCalculator {
     List<dynamic> li = text.split(RegExp('[$delimiter\n]'));
     for (int i = 0; i < li.length; i++) {
       if (int.tryParse(li[i]) != null) {
-        res = res + int.parse(li[i]);
+        int temp = int.parse(li[i]);
+        if (temp < 0) {
+          String op = '';
+          for (int j = i; j < li.length; j++) {
+            if (int.tryParse(li[j]) != null && int.tryParse(li[j])! < 0) {
+              op += '${li[j]},';
+            }
+          }
+          throw FormatException('Negative numbers not allowed: ${op.substring(0, op.length - 1)}');
+        } else {
+          res = res + int.parse(li[i]);
+        }
       }
     }
     return res;
